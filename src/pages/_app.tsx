cffbb263982e24819b564312d0d5a169f1b45d2e
lucas bandeira <lucas4workl8tr@gmail.com>
2023-03-14 18:15:35 -0300

    adicionado componente Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
-import type { AppProps } from 'next/app'
+import { Header } from '@/components/Header';
+import type {AppProps} from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/global.scss'
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+      <>
+        <Header/>
+        <Component {...pageProps} />
+      </>
+  );
 }
