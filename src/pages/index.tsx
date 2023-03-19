bd317f019452d99b14ee114b99830134dd662bca
lucas bandeira <lucas4workl8tr@gmail.com>
2023-03-18 23:46:54 -0300

    pagina de home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,84 @@
-export default function Home() {
+import { GetServerSideProps, GetStaticProps } from 'next';
+import { SubscribeButton } from '@/components/SubscribeButton';
+import Head from 'next/head';
+
+import styles from './home.module.scss';
+import {stripe} from '../services/stripe';
+
+interface HomeProps{
+  product:{
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
-    <h1>Hello world</h1>
+      <>
+        <Head>
+          <title> Home | ig.news </title>
+        </Head>
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publication <br/>
+              <span>for {product.amount} month</span>
+            </p>
+            <SubscribeButton priceId={product.priceId}/>
+          </section>
+
+          <img src="/images/avatar.svg" alt=" girl coding"/>
+        </main>
+      </>
+
   )
 }
+
+// static site generation
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Mmf33JjMXOQq4Q0rYBAaSt6', {
+    expand: ['product']
+  })
+
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      //@ts-ignore
+    }).format(price?.unit_amount / 100),
+  }
+
+  return{
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24//24 hours
+  }
+}
+
+// server side rendering
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1Mmf33JjMXOQq4Q0rYBAaSt6', {
+//     expand: ['product']
+//   })
+//
+//
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//       //@ts-ignore
+//     }).format(price?.unit_amount / 100),
+//   }
+//
+//   return{
+//     props: {
+//       product
+//     }
+//   }
+// }
